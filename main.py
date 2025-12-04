98514c1680b4a6d16dfceafe9f30a3071c805fb8
alisher.baidybekov <alisher.baidybekov@aviata.bisuness>
2025-12-04 18:40:16 +0500

    add deepsort

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ultralytics import YOLO
 import json
 import numpy as np
 import time
+from deep_sort_realtime.deepsort_tracker import DeepSort
 
 
 def load_zones():
@@ -12,10 +13,13 @@ def load_zones():
 
 
 def main():
-    model = YOLO('yolov8n.pt')
+    model = YOLO('yolov8m.pt')
     cap = cv2.VideoCapture('test.mp4')
+
     zone_array32 = load_zones()
     last_alarm_time = 0
+
+    tracker = DeepSort(max_age=300, embedder_gpu=True)
     
     while True:
         ret, frame = cap.read()
@@ -23,22 +27,45 @@ def main():
             break
         
         results = model(frame, verbose=False)
-        intrusion = False
+
+        detections = []
         
         for result in results:
-            boxes = result.boxes
-            for box in boxes:
+            for box in result.boxes:
                 if int(box.cls[0]) == 0:
-                    x1, y1, x2, y2 = [int(x) for x in box.xyxy[0]]
-                    center_x = (x1 + x2) // 2
-                    center_y = (y1 + y2) // 2
-                    
-                    if cv2.pointPolygonTest(zone_array32, (center_x, center_y), False) >= 0:
-                        intrusion = True
-                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                    else:
-                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    x1, y1, x2, y2 = box.xyxy[0].tolist()
+                    conf = box.conf[0].item()
+                    detections.append([[x1, y1, x2 - x1, y2 - y1], conf, 0])
         
+        tracks = tracker.update_tracks(detections, frame=frame)
+        intrusion = False
+        
+        for track in tracks:
+            if not track.is_confirmed() or track.time_since_update > 5:
+                continue
+            
+            
+            x1, y1, x2, y2 = [int(x) for x in track.to_ltrb()]
+            center_x = (x1 + x2) // 2
+            center_y = (y1 + y2) // 2
+
+            track_id = track.track_id
+
+            rectangle_args = [frame, (x1, y1), (x2, y2), 'color', 2]
+            puttext_args = [frame, f'ID:{track_id}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, 'color', 2]
+            
+            if cv2.pointPolygonTest(zone_array32, (center_x, center_y), False) >= 0:
+                intrusion = True
+                color = (0, 0, 255)
+            else:
+                color = (0, 255, 0)
+
+            rectangle_args[3] = color
+            puttext_args[5] = color
+
+            cv2.rectangle(*rectangle_args)
+            cv2.putText(*puttext_args)
+
         if intrusion:
             last_alarm_time = time.time()
         
